a1beb93e56413b4ec169b19b3704666b89180b4b
Prevail24 <hessedarren@gmail.com>
2019-03-08 16:13:50 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Initilize and add the map
+function initMap() {
+  // Your Location
+  const loc = { lat: 39.7392, lng: -104.9903};
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 9,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky Navbar menu opacity
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }  
+});
+
+// Smooth Scrolling
+$('#navbar a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
